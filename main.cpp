ef813f7c90a21ab69ee669e8e24aa97857ff65cf
Nivek-12 <kevin.jimenez.ara@gmail.com>
2024-09-22 21:18:45 -0600

    Comments

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ vector<int> genVector(int m, int n) {
     sort(vec.begin(),vec.end());
     return vec;
 }
+
+// Función para generar un vector con la secuencia de Fibonacci
 vector<int> genFibonacci(int limit) {
     vector<int> fib = {0, 1};
     while (true) {
@@ -27,17 +29,18 @@ vector<int> genFibonacci(int limit) {
     return fib;
 }
 
+// Función para encontrar los numeros de fibonacci en el vector random sin repetidos
 vector<int> findCoincidences(const vector<int>& vec, const vector<int>& fib) {
     vector<int> result;
     for (int num : vec) {
-        auto n = find(fib.begin(), fib.end(), num);
-        if (n != fib.end()) {
+        auto n = find(fib.begin(), fib.end(), num); // Verificacion de numero del vector random en fib
+        if (n != fib.end()) { // Verificacion ya que si no se encuentra devuelve el final de fib
             result.push_back(num);
         }
     }
     auto it = unique(result.begin(), result.end()); // Eliminacion de duplicados
     result.resize(distance(result.begin(), it));
-    result.insert(result.begin() + 1,1);
+    result.insert(result.begin() + 1,1); //Se agrega el 1 ya que se repite en la secuencia
     return result;
 }
 
